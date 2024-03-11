f34c4fd94c21eba9a0beb575fc4ada000f3c02c7
Faiza1st <faixa.faisal@gmail.com>
2024-03-11 18:38:08 +1100

    Schema file updated with primary key

--- db/schema.sql
+++ db/schema.sql
@@ -5,61 +5,51 @@ USE budget_db;
 
 -- Create the department table with primary key department_id
 CREATE TABLE categories (
-    categories_id INT AUTO_INCREMENT,
+    categories_id INT AUTO_INCREMENT PRIMARY KEY ,
     title  VARCHAR(30) NOT NULL,
     amount DECIMAL(10, 2),
-
-    PRIMARY KEY (title)
 );
 
 
 -- Create the role table reference department id
 CREATE TABLE budget (
-    id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     amount DECIMAL(10, 2) NOT NULL,
+    
     categories VARCHAR (30),
-    weeks VARCHAR(30) NOT NULL, 
-
-    PRIMARY KEY (id),
+    FOREIGN KEY (categories_id) 
+    REFERENCES categories(id), 
     
-    FOREIGN KEY (categories) 
-    REFERENCES categories(title) 
-    
-    FOREIGN KEY (weeks),
-    REFERENCES weeks(name),
+    weeks VARCHAR(30) NOT NULL,
+    FOREIGN KEY (weeks_id)
+    REFERENCES weeks(id),
 
-    ON DELETE SET NULL 
+    ON DELETE SET NULL, 
 );
 
 
 -- Create the employee table and the interconnection to role id
 CREATE TABLE weeks (
-    id INTEGER AUTO_INCREMENT,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
-    total DECIMAL (10, 2),
-    
-   PRIMARY KEY (name),
+    total DECIMAL (10, 2) ,
    
-   ON DELETE SET NULL
 );
 
 -- Create the manager table and connect the reference to employee id
 CREATE TABLE goals (
-    id INT AUTO_INCREMENT
+    id INT AUTO_INCREMENT PRIMARY KEY ,
     weekly_total DECIMAL (10, 2),
-    category VARCHAR(30),
     category_amount DECIMAL (10, 2),
-    weeks VARCHAR(30),
-
-    PRIMARY KEY (id),
-       
-    FOREIGN KEY (categories),
-    REFERENCES categories(title),
-
-    FOREIGN KEY (weeks),
-    REFERENCES week(name),
 
 
+    category VARCHAR(30) 
+    FOREIGN KEY (categories_id)
+    REFERENCES categories(id),
     ON DELETE SET NULL
+
+    weeks VARCHAR(30)
+    FOREIGN KEY (weeks_id)
+    REFERENCES week(id),
 );
