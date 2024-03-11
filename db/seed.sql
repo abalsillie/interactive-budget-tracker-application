0b308e11b821c28bc84806dfdd4cfe16e20ff017
Faiza1st <faixa.faisal@gmail.com>
2024-03-11 19:28:17 +1100

    publci file fixed

---
+++ db/seed.sql
@@ -0,0 +1,13 @@
+INSERT INTO categories (title)
+VALUES
+('Utilities'),
+('Health care'),
+('Insurance'),
+('Rent'),
+('Food'),
+('Personal spending'),
+('Mortgage')
+
+INSERT INTO budget (title)
+VALUES
+
