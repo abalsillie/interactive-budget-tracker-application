26b29f299e1eb4d449c7dffb6d6d438ab7d797d5
Lex <info@alexandranel.com>
2024-03-14 20:34:30 +1100

    relationships updated and routes to models updated

--- controllers/api/Spends.js
+++ controllers/api/Spends.js
@@ -1,36 +1,36 @@
 const router = require('express').Router();
-const { Projects } = require('../../models');
+const { Spends } = require('../../models/Spends');
 const withAuths = require('../../utils/auth');
 
 router.post('/', withAuths, async (req, res) => {
     try {
-      const newProjects = await Projects.create({
+      const newSpends = await Spends.create({
         ...req.body,
         user_id: req.session.user_id,
       });
   
-      res.status(200).json(newProjects);
+      res.status(200).json(newSpends);
     } 
     catch (err) {
-      res.status(400).json(err);
+      res.status(404).json({ message: 'Error creating new Spends!' });
     }
 });
 
 router.delete('/:id', withAuths, async (req, res) => {
     try {
-      const projectsData = await Projects.destroy({
+      const spendsData = await Spends.destroy({
         where: {
           id: req.params.id,
           user_id: req.session.user_id,
         },
       });
   
-      if (!projectsData) {
-        res.status(404).json({ message: 'No project found with this id!' });
+      if (!spendsData) {
+        res.status(404).json({ message: 'No spends data with this id is found!' });
         return;
       }
   
-      res.status(200).json(projectsData);
+      res.status(200).json(spendsData);
     } catch (err) {
       res.status(500).json(err);
     }
