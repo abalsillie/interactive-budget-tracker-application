1d00adcefd8d556a003cd13048aaab78ebf8982c
Lex <info@alexandranel.com>
2024-03-16 10:40:21 +1100

    have updated routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
-const routesUser = require('./userroutes');
-const routesProjects = require('./routesProjects.js');
+const routesUser = require('./routesUser');
+const routesCategories = require('./routesCategories');
+const routesSpends = require('./routesSpends');
+const routesWeeks = require('./routesWeeks');
+const routesGoals = require('./routesGoals');
 
-router.use('/users', routesUser);
-router.use('/projects', routesProjects);
+
+router.use('/user', routesUser);
+router.use('/categories', routesCategories);
+router.use('/spends', routesSpends);
+router.use('/weeks', routesWeeks);
+router.use('/goals', routesGoals);
 
 module.exports = router;
